fb8055a779068ddae14f90259e7e3db4e6b04e19
Jim Neath <jim@virtuaffinity.net>
2008-08-11 15:34:19 +0100

    Fixed some formatting in generated files

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,24 +1,23 @@
 class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
-    @subject    += 'Please activate your new account'
-  
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
-  
+    @subject << 'Please activate your new account'
+    @body[:url] = "http://YOURSITE/activate/#{user.activation_code}"
   end
   
   def activation(user)
     setup_email(user)
-    @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @subject << 'Your account has been activated!'
+    @body[:url] = "http://YOURSITE/"
   end
   
   protected
-    def setup_email(user)
-      @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
-      @sent_on     = Time.now
-      @body[:user] = user
-    end
+  
+  def setup_email(user)
+    @recipients = "#{user.email}"
+    @from = "ADMINEMAIL"
+    @subject = "[YOURSITE] "
+    @sent_on = Time.now
+    @body[:user] = user
+  end
 end
