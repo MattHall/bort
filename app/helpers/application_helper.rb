fa34dea2fd1bb387cf7a6d725e461cb0063a6f84
Jim Neath <jim@virtuaffinity.net>
2008-08-11 16:03:57 +0100

    Fixed typo in title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   def title(str, container = nil)
     @page_title = str
-    container_tag(container, str) if container
+    content_tag(container, str) if container
   end
   
 end
