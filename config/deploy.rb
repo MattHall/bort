50be401beef7935898a318c49645993bd0be98f2
Jim Neath <jim@virtuaffinity.net>
2008-08-11 15:23:48 +0100

    Added basic cap recipe to work with git and passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,54 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+#############################################################
+#	Application
+#############################################################
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :application, "bort"
+set :deploy_to, "/path/to/deploy"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+#############################################################
+#	Settings
+#############################################################
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :use_sudo, true
+set :scm_verbose, true 
+
+#############################################################
+#	Servers
+#############################################################
+
+set :user, "bort"
+set :domain, "www.example.com"
+server domain, :app, :web
+role :db, domain, :primary => true
+
+#############################################################
+#	Git
+#############################################################
+
+set :scm, :git
+set :branch, "master"
+set :scm_user, 'bort'
+set :scm_passphrase, "PASSWORD"
+set :repository, "git@github.com:FudgeStudios/bort.git"
+set :deploy_via, :remote_cache
+
+#############################################################
+#	Passenger
+#############################################################
+
+namespace :deploy do
+  
+  # Restart passenger on deploy
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+  
+end
