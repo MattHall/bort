44ec3f7b221903ad0465e9fd6b437dec8fdf2c94
Phil Jeffs <phil@fudgestudios.com>
2008-09-29 20:29:25 +0100

    added multistage capistrano deployment stuff

--- config/environment.rb
+++ config/environment.rb
@@ -22,9 +22,9 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on. 
   # They can then be installed with "rake gems:install" on new installations.
-  # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
+config.gem "capistrano-ext", :lib => "capistrano"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
