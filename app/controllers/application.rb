49d57bac877ebcaa54c00d71b66dd3d064150627
Jim Neath <jim@virtuaffinity.net>
2008-10-04 09:54:23 +0100

    Added record_not_found action to application.rb to automatically respond with a 404 when records cannot be found

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,5 +6,13 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery :secret => 'b0a876313f3f9195e9bd01473bc5cd06'
   filter_parameter_logging :password, :password_confirmation
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+  
+  protected
+  
+  # Automatically respond with 404 for ActiveRecord::RecordNotFound
+  def record_not_found
+    render :file => File.join(RAILS_ROOT, 'public', '404.html'), :status => 404
+  end
 end
 
