6caa6e577d410e15c3112dc00e01e07130fda1e2
Jim Neath <jim@virtuaffinity.net>
2008-10-07 10:26:58 +0100

    Moved AuthenticatedTestHelper and AuthenticatedSystem to spec_helper.rb

--- spec/controllers/authenticated_system_spec.rb
+++ spec/controllers/authenticated_system_spec.rb
@@ -1,10 +1,5 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-# Be sure to include AuthenticatedTestHelper in spec/spec_helper.rb instead
-# Then, you can remove it from this and the units test.
-include AuthenticatedTestHelper
-include AuthenticatedSystem
-
 describe SessionsController do
   fixtures :users
   
