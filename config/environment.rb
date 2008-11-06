612a1c930b24daa7d08cbd6cc560ef785d83d1b1
Jim Neath <jim@virtuaffinity.net>
2008-11-06 12:33:45 +0000

    Removed will_paginate and aasm plugins and installed gems instead. Go gems!

--- config/environment.rb
+++ config/environment.rb
@@ -30,7 +30,9 @@ Rails::Initializer.run do |config|
   config.gem "capistrano-ext", :lib => "capistrano"
   config.gem 'rspec-rails', :lib => 'spec/rails', :version => '1.1.11'
   config.gem 'rspec', :lib => 'spec', :version => '1.1.11'
-
+  config.gem 'rubyist-aasm', :lib => 'aasm', :source => 'http://gems.github.com', :version => '2.0.2'
+  config.gem 'mislav-will_paginate', :version => '2.3.6', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
--- vendor/plugins/aasm/spec/functional/conversation_spec.rb
+++
@@ -1,5 +0,0 @@
-describe Conversation, 'description' do
-  it '.aasm_states should contain all of the states' do
-    Conversation.aasm_states.should == [:needs_attention, :read, :closed, :awaiting_response, :junk]
-  end
-end
