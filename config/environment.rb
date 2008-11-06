4fcfeb523b2c271ccf07390b25680c903a199a93
Jim Neath <jim@virtuaffinity.net>
2008-11-06 12:16:45 +0000

    Removed rspec and rspec-plugins and replaced with newest gems and unpacked

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "capistrano-ext", :lib => "capistrano"
+  config.gem 'rspec-rails', :lib => 'spec/rails', :version => '1.1.11'
+  config.gem 'rspec', :lib => 'spec', :version => '1.1.11'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
