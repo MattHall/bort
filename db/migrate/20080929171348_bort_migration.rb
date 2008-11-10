098c945e0253bca6295b316038a5458167e04221
Matt Hall <matthew.hall@gmail.com>
2008-11-10 09:53:23 +0000

    Updated migration to use new bort migration

--- db/migrate/20080929171348_bort_migration.rb
+++ db/migrate/20080929171348_bort_migration.rb
@@ -25,7 +25,6 @@ class BortMigration < ActiveRecord::Migration
     
     # Create Users Table
     create_table :users do |t|
-      t.string :login, :limit => 40
       t.string :identity_url      
       t.string :name, :limit => 100, :default => '', :null => true
       t.string :email, :limit => 100
