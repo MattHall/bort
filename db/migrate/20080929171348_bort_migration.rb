aa502662b41eaa8f5bda240f8e2debd49564170f
Matt Hall <matthew.hall@gmail.com>
2008-11-12 09:36:03 +0000

    Fixed migration

--- db/migrate/20080929171348_bort_migration.rb
+++ db/migrate/20080929171348_bort_migration.rb
@@ -39,7 +39,7 @@ class BortMigration < ActiveRecord::Migration
       t.timestamps
     end
     
-    add_index :users, :login, :unique => true
+    add_index :users, :email, :unique => true
     
     # Create Passwords Table
     create_table :passwords do |t|
@@ -64,7 +64,6 @@ class BortMigration < ActiveRecord::Migration
     
     # Create default admin user
     user = User.create do |u|
-      u.login = 'admin'
       u.password = u.password_confirmation = 'chester'
       u.email = APP_CONFIG[:admin_email]
     end
